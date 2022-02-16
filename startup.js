00ffa02e545596054129d5289c2d4451cb02a6d7
Emina Bašić <ebasic3@etf.unsa.ba>
2022-02-16 18:43:53 +0100

    deleted bodyParser

--- startup.js
+++ startup.js
@@ -1,15 +1,12 @@
-const bodyParser = require('body-parser');
 const express = require('express');
 
 const app = express();
 const port = process.env.PORT;
-app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.json({ status: 'Prvi prolaz :)' });
 });
 
 app.use(require('./app/routes'));
-
 
 app.listen(port);
