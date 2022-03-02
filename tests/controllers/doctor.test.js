31a7739ca385f08d99982df94b3a60ee7b41fc44
Emina Bašić <ebasic3@etf.unsa.ba>
2022-03-02 19:48:02 +0100

    tests for GET arranged

--- tests/controllers/doctor.test.js
+++ tests/controllers/doctor.test.js
@@ -115,236 +115,108 @@ describe("POST doctors", async () => {
     expect(body.error).to.equals("Doctor with this email already exists");
   });
 });
-// describe("GET doctors", async () => {
-//   beforeAll(async () => {
-//     await db.DoctorPracticeArea.destroy({ truncate: { cascade: true } });
-//     await db.WorkingHours.destroy({ truncate: { cascade: true } });
-//     await db.PracticeArea.destroy({ truncate: { cascade: true } });
-//     await db.Doctor.destroy({ truncate: { cascade: true } });
-//   });
-//   it("Get all doctors", async () => {
-//     const pa = await db.PracticeArea.create({
-//       id: 3,
-//       name: "Dermatology",
-//       created_at: new Date(),
-//       updated_at: new Date(),
-//     });
-//     await request(app)
-//       .post("/api/doctors")
-//       .send({
-//         image: "https://upload.wikimedia.org",
-//         title: "Mr.Prim.Prof.Dr.",
-//         firstName: "Jane",
-//         lastName: "Driver",
-//         practiceArea: [pa.id],
-//         adress: "Sarajevska 71",
-//         city: "Sarajevo",
-//         zip: "71000",
-//         country: "Bosnia and Hercegovina",
-//         phoneNumber: "123-060-459",
-//         website: "www.house.com",
-//         workingHours: [
-//           { day: "monday", workTimeStart: "08:00", workTimeEnd: "18:00" },
-//           { day: "tuesday", workTimeStart: "08:00", workTimeEnd: "18:00" },
-//           { day: "wednesday", workTimeStart: "08:00", workTimeEnd: "18:00" },
-//         ],
-//         email: "jane@atrijum.ba",
-//       });
-//     await request(app)
-//       .post("/api/doctors")
-//       .send({
-//         image: "https://upload.wikimedia.org",
-//         title: "Mr.Prim.Prof.Dr.",
-//         firstName: "Joe",
-//         lastName: "Driver",
-//         practiceArea: [pa.id],
-//         adress: "Sarajevska 71",
-//         city: "Sarajevo",
-//         zip: "71000",
-//         country: "Bosnia and Hercegovina",
-//         phoneNumber: "123-060-459",
-//         website: "www.house.com",
-//         workingHours: [
-//           { day: "monday", workTimeStart: "08:00", workTimeEnd: "18:00" },
-//           { day: "tuesday", workTimeStart: "08:00", workTimeEnd: "18:00" },
-//           { day: "wednesday", workTimeStart: "08:00", workTimeEnd: "18:00" },
-//         ],
-//         email: "joe@atrijum.ba",
-//       });
 
-//     const response = await request(app).get("/api/doctors?");
-//     expect(response.status).to.equal(201);
-//     expect(response.body).to.have.lengthOf(2);
-//   });
+describe("GET doctors", async () => {
+  beforeEach(async () => {
+    await db.DoctorPracticeArea.destroy({ truncate: { cascade: true } });
+    await db.WorkingHours.destroy({ truncate: { cascade: true } });
+    await db.PracticeArea.destroy({ truncate: { cascade: true } });
+    await db.Doctor.destroy({ truncate: { cascade: true } });
 
-//   it("Get all doctors with name Joe", async () => {
-//     const pa = await db.PracticeArea.create({
-//       id: 4,
-//       name: "Dermatology",
-//       created_at: new Date(),
-//       updated_at: new Date(),
-//     });
-//     await request(app)
-//       .post("/api/doctors")
-//       .send({
-//         image: "https://upload.wikimedia.org",
-//         title: "Mr.Prim.Prof.Dr.",
-//         firstName: "Jane",
-//         lastName: "Driver",
-//         practiceArea: [pa.id],
-//         adress: "Sarajevska 71",
-//         city: "Sarajevo",
-//         zip: "71000",
-//         country: "Bosnia and Hercegovina",
-//         phoneNumber: "123-060-459",
-//         website: "www.house.com",
-//         workingHours: [
-//           { day: "monday", workTimeStart: "08:00", workTimeEnd: "18:00" },
-//           { day: "tuesday", workTimeStart: "08:00", workTimeEnd: "18:00" },
-//           { day: "wednesday", workTimeStart: "08:00", workTimeEnd: "18:00" },
-//         ],
-//         email: "jane@atrijum.ba",
-//       });
-//     await request(app)
-//       .post("/api/doctors")
-//       .send({
-//         image: "https://upload.wikimedia.org",
-//         title: "Mr.Prim.Prof.Dr.",
-//         firstName: "Joe",
-//         lastName: "Driver",
-//         practiceArea: [pa.id],
-//         adress: "Sarajevska 71",
-//         city: "Sarajevo",
-//         zip: "71000",
-//         country: "Bosnia and Hercegovina",
-//         phoneNumber: "123-060-459",
-//         website: "www.house.com",
-//         workingHours: [
-//           { day: "monday", workTimeStart: "08:00", workTimeEnd: "18:00" },
-//           { day: "tuesday", workTimeStart: "08:00", workTimeEnd: "18:00" },
-//           { day: "wednesday", workTimeStart: "08:00", workTimeEnd: "18:00" },
-//         ],
-//         email: "joe@atrijum.ba",
-//       });
+    const practiceArea = await db.PracticeArea.create({
+      id: 3,
+      name: "Dermatology",
+      created_at: new Date(),
+      updated_at: new Date(),
+    });
+    const workingHours = [
+      { day: "monday", workTimeStart: "08:00", workTimeEnd: "18:00" },
+      { day: "tuesday", workTimeStart: "08:00", workTimeEnd: "18:00" },
+      { day: "wednesday", workTimeStart: "08:00", workTimeEnd: "18:00" },
+    ];
+    const doctor1 = await db.Doctor.create({
+      image: "https://upload.wikimedia.org",
+      title: "Mr.Prim.Prof.Dr.",
+      firstName: "Jane",
+      lastName: "Driver",
+      adress: "Sarajevska 71",
+      city: "Sarajevo",
+      zip: "71000",
+      country: "Bosnia and Hercegovina",
+      phoneNumber: "123-060-459",
+      website: "www.house.com",
+      email: "jane@atrijum.ba",
+    });
+    const doctor2 = await db.Doctor.create({
+      image: "https://upload.wikimedia.org",
+      title: "Mr.Prim.Prof.Dr.",
+      firstName: "Joe",
+      lastName: "Driver",
+      adress: "Sarajevska 71",
+      city: "Sarajevo",
+      zip: "71000",
+      country: "Bosnia and Hercegovina",
+      phoneNumber: "123-060-459",
+      website: "www.house.com",
+      email: "joe@atrijum.ba",
+    });
 
-//     const response = await request(app).get("/api/doctors?name=Joe");
-//     expect(response.status).to.equal(201);
-//     expect(response.body).to.have.lengthOf(1);
-//   });
-//   it("Get all doctors with practice area Dermatology", async () => {
-//     const pa1 = await db.PracticeArea.create({
-//       id: 4,
-//       name: "Dermatology",
-//       created_at: new Date(),
-//       updated_at: new Date(),
-//     });
-//     const pa2 = await db.PracticeArea.create({
-//       id: 1,
-//       name: "Surgeon",
-//       created_at: new Date(),
-//       updated_at: new Date(),
-//     });
-//     await request(app)
-//       .post("/api/doctors")
-//       .send({
-//         image: "https://upload.wikimedia.org",
-//         title: "Mr.Prim.Prof.Dr.",
-//         firstName: "Jane",
-//         lastName: "Driver",
-//         practiceArea: [pa1.id],
-//         adress: "Sarajevska 71",
-//         city: "Sarajevo",
-//         zip: "71000",
-//         country: "Bosnia and Hercegovina",
-//         phoneNumber: "123-060-459",
-//         website: "www.house.com",
-//         workingHours: [
-//           { day: "monday", workTimeStart: "08:00", workTimeEnd: "18:00" },
-//           { day: "tuesday", workTimeStart: "08:00", workTimeEnd: "18:00" },
-//           { day: "wednesday", workTimeStart: "08:00", workTimeEnd: "18:00" },
-//         ],
-//         email: "jane@atrijum.ba",
-//       });
-//     await request(app)
-//       .post("/api/doctors")
-//       .send({
-//         image: "https://upload.wikimedia.org",
-//         title: "Mr.Prim.Prof.Dr.",
-//         firstName: "Joe",
-//         lastName: "Driver",
-//         practiceArea: [pa2.id],
-//         adress: "Sarajevska 71",
-//         city: "Sarajevo",
-//         zip: "71000",
-//         country: "Bosnia and Hercegovina",
-//         phoneNumber: "123-060-459",
-//         website: "www.house.com",
-//         workingHours: [
-//           { day: "monday", workTimeStart: "08:00", workTimeEnd: "18:00" },
-//           { day: "tuesday", workTimeStart: "08:00", workTimeEnd: "18:00" },
-//           { day: "wednesday", workTimeStart: "08:00", workTimeEnd: "18:00" },
-//         ],
-//         email: "joe@atrijum.ba",
-//       });
+    workingHours.forEach(async (workingHours1) => {
+      await db.WorkingHours.create({
+        ...workingHours1,
+        doctor_id: doctor1.id,
+      });
+    });
 
-//     const response = await request(app).get("/api/doctors?practiceArea=4");
-//     expect(response.status).to.equal(201);
-//     expect(response.body).to.have.lengthOf(1);
-//   });
-//   it("Get all doctors where practice area is Dermatology and name is Joe", async () => {
-//     const pa = await db.PracticeArea.create({
-//       id: 4,
-//       name: "Dermatology",
-//       created_at: new Date(),
-//       updated_at: new Date(),
-//     });
-//     await request(app)
-//       .post("/api/doctors")
-//       .send({
-//         image: "https://upload.wikimedia.org",
-//         title: "Mr.Prim.Prof.Dr.",
-//         firstName: "Jane",
-//         lastName: "Driver",
-//         practiceArea: [pa.id],
-//         adress: "Sarajevska 71",
-//         city: "Sarajevo",
-//         zip: "71000",
-//         country: "Bosnia and Hercegovina",
-//         phoneNumber: "123-060-459",
-//         website: "www.house.com",
-//         workingHours: [
-//           { day: "monday", workTimeStart: "08:00", workTimeEnd: "18:00" },
-//           { day: "tuesday", workTimeStart: "08:00", workTimeEnd: "18:00" },
-//           { day: "wednesday", workTimeStart: "08:00", workTimeEnd: "18:00" },
-//         ],
-//         email: "jane@atrijum.ba",
-//       });
-//     await request(app)
-//       .post("/api/doctors")
-//       .send({
-//         image: "https://upload.wikimedia.org",
-//         title: "Mr.Prim.Prof.Dr.",
-//         firstName: "Joe",
-//         lastName: "Driver",
-//         practiceArea: [pa.id],
-//         adress: "Sarajevska 71",
-//         city: "Sarajevo",
-//         zip: "71000",
-//         country: "Bosnia and Hercegovina",
-//         phoneNumber: "123-060-459",
-//         website: "www.house.com",
-//         workingHours: [
-//           { day: "monday", workTimeStart: "08:00", workTimeEnd: "18:00" },
-//           { day: "tuesday", workTimeStart: "08:00", workTimeEnd: "18:00" },
-//           { day: "wednesday", workTimeStart: "08:00", workTimeEnd: "18:00" },
-//         ],
-//         email: "joe@atrijum.ba",
-//       });
+    await db.DoctorPracticeArea.create({
+      doctor_id: doctor1.id,
+      practice_area_id: practiceArea.id,
+    });
 
-//     const response = await request(app).get(
-//       "/api/doctors?name=Joe&practiceArea=4"
-//     );
-//     expect(response.status).to.equal(201);
-//     expect(response.body).to.have.lengthOf(1);
-//   });
-// });
+    workingHours.forEach(async (workingHours1) => {
+      await db.WorkingHours.create({
+        ...workingHours1,
+        doctor_id: doctor2.id,
+      });
+    });
+
+    await db.DoctorPracticeArea.create({
+      doctor_id: doctor2.id,
+      practice_area_id: practiceArea.id,
+    });
+  });
+  it("Get all doctors", async () => {
+    // act
+    const response = await request(app).get("/api/doctors?");
+
+    // assert
+    expect(response.status).to.equal(201);
+    expect(response.body).to.have.lengthOf(2);
+  });
+
+  it("Get all doctors with name Joe", async () => {
+    // act
+    const response = await request(app).get("/api/doctors?name=Joe");
+
+    // assert
+    expect(response.status).to.equal(201);
+    expect(response.body).to.have.lengthOf(1);
+  });
+  it("Get all doctors with practice area Dermatology", async () => {
+    // act
+    const response = await request(app).get("/api/doctors?practiceArea=3");
+
+    // assert
+    expect(response.status).to.equal(201);
+    expect(response.body).to.have.lengthOf(2);
+  });
+  it("Get all doctors where practice area is Dermatology and name is Joe", async () => {
+    // act
+    const response = await request(app).get(
+      "/api/doctors?name=Joe&practiceArea=3"
+    );
+    // assert
+    expect(response.status).to.equal(201);
+    expect(response.body).to.have.lengthOf(1);
+  });
+});
