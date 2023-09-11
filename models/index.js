13242ed65a7f9e7ec747ce9940175c8c1fea32ce
Emina Basic <ebasic3@etf.unsa.ba>
2023-09-11 13:32:50 +0200

    env for db config

--- models/index.js
+++ models/index.js
@@ -1,45 +1,33 @@
 const fs = require("fs");
 const path = require("path");
 const Sequelize = require("sequelize");
+const config = require("../config/config");
 
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
-const config = require(`${__dirname}/../config/config.json`)[env];
 
 const db = {};
 
 let sequelize;
 
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+if (config[env].use_env_variable) {
+  sequelize = new Sequelize(process.env[config[env].use_env_variable], config[env]);
 } else {
   sequelize = new Sequelize(
-    config.database,
-
-    config.username,
-
-    config.password,
-
+    config[env].database,
+    config[env].username,
+    config[env].password,
     {
-      ...config,
+      ...config[env],
       logging: false,
     }
   );
 }
 
 fs.readdirSync(__dirname)
-
-  .filter(
-    (file) => file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
-  )
-
+  .filter((file) => file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js")
   .forEach((file) => {
-    const model = require(path.join(__dirname, file))(
-      sequelize,
-
-      Sequelize.DataTypes
-    );
-
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
   });
 
